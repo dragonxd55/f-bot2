18aa491673c92f519e6b4e04cdf889a5dcabef25
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-03-29 04:08:46 +0200

    Update icon.js

--- cogs/icon.js
+++ cogs/icon.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
-  var copy = "All CopyRights Reseaved For Luxury bot team";
-  var link = "https://discord.gg/tjnFVzy";
-  var img = "http://codeup.tk/s/do.php?img=26";
-  var idc = "419258975650054145";
+  var copy = "All CopyRights Server Fabulous";
+  var link = "https://discord.gg/e9Ba7GK";
+  var img = "https://media.discordapp.net/attachments/374950872679186433/428733009458495488/image.jpg?width=498&height=486";
+  var idc = "428733432731009024";
   var vr = 'Version  : 1.0.0';
 module.exports.run = async (bot, message, args) => {
     if(!message.channel.guild) return;
