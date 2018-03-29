faab53b72819d04b4e8ae780e7f59b0b2a4210c8
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-03-29 04:12:59 +0200

    Update info.js

--- cogs/info.js
+++ cogs/info.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
-  var copy = "All CopyRights Reseaved For Luxury bot team";
-  var link = "https://discord.gg/tjnFVzy";
-  var img = "http://codeup.tk/s/do.php?img=26";
-  var idc = "419258975650054145";
+  var copy = "All CopyRights Server Fabulous";
+  var link = "https://discord.gg/e9Ba7GK";
+  var img = "https://media.discordapp.net/attachments/374950872679186433/428733009458495488/image.jpg?width=498&height=486";
+  var idc = "428733432731009024";
   var vr = 'Version  : 1.2.0';
 module.exports.run = async (bot, message, args) => {
           if(!message.channel.guild) return message.reply('**انتا تبغا توقف البوت ولا تستهبل رح السيرفر تشتغل قفطتك هاه يا ذكي**');
@@ -13,7 +13,7 @@ module.exports.run = async (bot, message, args) => {
          .addField("`` الرام المستخدم للجافا : ``",`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`,true)
          .addField("``عدد المستخدمين الحاليين``",`${bot.guilds.reduce((mem, g) => mem += g.memberCount, 0)}`,true)
          .addField("``اصداري :``", `${vr}`,true)
-         .addField("``bot Luxury bot 💎 ID :``", "ID : ``419188357181079553`` ",true)
+         .addField("``Fabulous 💎 ID :``", "ID : ``428733432731009024`` ",true)
          .addField("``لايمكن الاعتماد عليها``",` سرعة الاستجابة! \`${Date.now() - message.createdTimestamp} ms\``,true)
          .addField("add " , link)
          .setColor('RANDOM')
